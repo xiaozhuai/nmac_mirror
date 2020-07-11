c432f74abd61229044d585e65086a8b80355f4f4
xiaozhuai <798047000@qq.com>
2020-07-11 22:06:41 +0800

    update build.sh

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# NMac Mirror
+[https://nmac.xiaozhuai.net/](https://nmac.xiaozhuai.net/)
+
 # Build
 
 Install `bindate` first
@@ -5,14 +8,16 @@ Install `bindate` first
 go get -u github.com/kataras/bindata/cmd/bindata
 ```
 
+Then
+
 ```
 ./build.sh
 ```
 
 # TODO
 
-*[x] frontend
-*[x] embed public files
-*[x] embed public gzipped files
-*[ ] github action
-*[ ] docker
+* [x] frontend
+* [x] embed public files
+* [x] embed public gzipped files
+* [ ] github action
+* [ ] docker
--- build.sh
+++ build.sh
@@ -26,3 +26,23 @@ CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -i -o ./build/nmac_mirror_win_a
 
 echo "Build nmac_mirror_freebsd_amd64 ..."
 CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -i -o ./build/nmac_mirror_freebsd_amd64 .
+
+rm -rf zip
+mkdir zip
+cp -f ./build/nmac_mirror_darwin_amd64 ./zip/
+cp -f ./build/nmac_mirror_linux_amd64 ./zip/
+cp -f ./build/nmac_mirror_linux_arm ./zip/
+cp -f ./build/nmac_mirror_linux_arm64 ./zip/
+cp -f ./build/nmac_mirror_win_386.exe ./zip/
+cp -f ./build/nmac_mirror_win_amd64.exe ./zip/
+cp -f ./build/nmac_mirror_freebsd_amd64 ./zip/
+cp -f config.yaml ./zip/
+
+cd zip
+zip nmac_mirror_darwin_amd64.zip config.yaml nmac_mirror_darwin_amd64
+zip nmac_mirror_linux_amd64.zip config.yaml nmac_mirror_linux_amd64
+zip nmac_mirror_linux_arm.zip config.yaml nmac_mirror_linux_arm
+zip nmac_mirror_linux_arm64.zip config.yaml nmac_mirror_linux_arm64
+zip nmac_mirror_win_386.zip config.yaml nmac_mirror_win_386.exe
+zip nmac_mirror_win_amd64.zip config.yaml nmac_mirror_win_amd64.exe
+zip nmac_mirror_freebsd_amd64.zip config.yaml nmac_mirror_freebsd_amd64
