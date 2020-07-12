0fd52014379eec4c84b1263d8554d4a98db941a6
xiaozhuai <798047000@qq.com>
2020-07-12 17:00:02 +0800

    optimize style for detail content

--- frontend/src/components/DetailDialog.vue
+++ frontend/src/components/DetailDialog.vue
@@ -14,26 +14,25 @@
                     <span class="detail-field">Posted: </span>
                     <span class="detail-value">{{detail.date_published}}</span>
                 </div>
-                <download-button v-if="detail.urls && detail.urls.length > 0" :urls="detail.urls"/>
-                <el-button v-else class="detail-download" type="primary" plain size="small">
+                <download-button class="detail-btn" v-if="detail.urls && detail.urls.length > 0" :urls="detail.urls"/>
+                <el-button v-else class="detail-download detail-btn" type="primary" plain size="small">
                     No Resource!
                 </el-button>
-                <br>
                 <el-button v-if="detail.previous_page_url && !previousVersionLoaded"
                            v-loading="previousLoading"
-                           class="detail-download"
+                           class="detail-btn detail-download download-button"
                            type="primary" plain size="small"
                            @click="onClickPreviousVersion">
                     Previous Version
                 </el-button>
                 <template v-if="previousVersionLoaded">
-                    <span v-if="previousVersions.length === 0" style="margin-top: 12px; display: block;">No Previous Version!</span>
-                    <div class="previous-version-container" v-else>
-                        <download-button v-for="(version, index) of previousVersions"
-                                         :key="index"
-                                         :title="version.version"
-                                         :urls="version.urls"/>
-                    </div>
+                    <el-button v-if="previousVersions.length === 0" class="detail-download detail-btn" type="primary" plain size="small">
+                        No Previous Version!
+                    </el-button>
+                    <download-button class="detail-btn" v-for="(version, index) of previousVersions"
+                                     :key="index"
+                                     :title="version.version"
+                                     :urls="version.urls"/>
                 </template>
 
                 <div class="detail-content" v-html="detail.content"></div>
@@ -91,8 +90,51 @@
         padding: 12px 12px;
     }
 
+    .detail-content {
+        margin-top: 24px;
+    }
+
     .detail-content img {
         max-width: 100%;
+    }
+
+    .detail-content .label-important {
+        padding: 4px 16px;
+        border: 1px solid #dddddd;
+        border-radius: 6px;
+        color: white;
+        background-color: #dd6161;
+    }
+
+    .detail-content blockquote {
+        border-color: #e3e3e3;
+        padding: 0 0 0 15px;
+        margin: 0 0 20px;
+        border-left: 5px solid #eee;
+    }
+
+    .detail-content blockquote p {
+        font-size: 12.5px;
+        font-weight: 200;
+        line-height: 1.25;
+    }
+
+    .detail-content code {
+        padding: 2px 4px;
+        color: #d14;
+        white-space: nowrap;
+        background-color: #f7f7f9;
+        border: 1px solid #e1e1e8;
+    }
+
+    code, pre {
+        padding: 0 3px 2px;
+        font-family: Monaco,Menlo,Consolas,courier new,monospace;
+        font-size: 12px;
+        color: #333;
+        -webkit-border-radius: 3px;
+        -moz-border-radius: 3px;
+        border-radius: 3px;
     }
 
     .el-dialog {
@@ -103,8 +145,8 @@
     .detail-icon {
         width: 120px;
         height: 120px;
-        margin-top: 16px;
-        margin-bottom: 4px;
+        float: left;
+        margin-right: 16px;
     }
 </style>
 
@@ -127,7 +169,7 @@
         margin-top: 12px;
     }
 
-    .previous-version-container .download-button {
+    .detail-btn {
         margin-right: 12px;
     }
 </style>
